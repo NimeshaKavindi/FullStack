bf7f4d82f568045ff27500d319ab29f63da6af19
Nimesha Kavindi <abeykoo-se19021@stu.kln.ac.lk>
2023-12-14 19:24:49 +0530

    Create postman and put data to mysql in JSON format

--- src/main/java/com/project/studentsystem/service/StudentService.java
+++ src/main/java/com/project/studentsystem/service/StudentService.java
@@ -1,4 +1,10 @@
 package com.project.studentsystem.service;
 
+import com.project.studentsystem.moodle.Student;
+
+import java.util.List;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
--- src/main/java/com/project/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/project/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,17 @@
 package com.project.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.project.studentsystem.moodle.Student;
+import com.project.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
